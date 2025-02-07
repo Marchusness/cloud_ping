718098e224815d20c63efd7bb2c518659541852a
Marchusness <m_gunny31@outlook.com>
2025-02-07 23:16:45 +1100

    update web for easier aws region selection

--- worker/src/handlers/fetch/shouldPingNewRegion.ts
+++ worker/src/handlers/fetch/shouldPingNewRegion.ts
@@ -2,6 +2,6 @@
 export function shouldPingNewRegion(count: number) {
   const prodOfNewPing = Math.pow(1.5, -(count / 100));
 
-  // At least 1% chance of recalculating
-  return Math.max(prodOfNewPing, 0.01) > Math.random();
+  // At least 5% chance of recalculating
+  return Math.max(prodOfNewPing, 0.05) > Math.random();
 }
